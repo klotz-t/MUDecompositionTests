4083b37ade323f08706cb8e35cca2d851216a317
klotz-t <thomas.klotz@mechbau.uni-stuttgart.de>
2024-03-20 22:22:14 +0100

    Catch statement if the separation vector contains NaN

--- pure-simulation-trials/functions/in_silico_decomposition_ica.m
+++ pure-simulation-trials/functions/in_silico_decomposition_ica.m
@@ -14,11 +14,13 @@ num_MUs = size(motor_unit_responses, 1);
 % Number of time samples of the motor unit responses
 L = size(motor_unit_responses,3);
 %% Signal Extension
+disp(['Extension'])
 % Extend the multi-cahnnel signal by the length of the MU responses.
 ext_SIG = extension(SIG, L);
 % Dimension of the extended signal
 [~ , ext_SIG_Dim] = size(ext_SIG);
 %% Whitening
+disp(['Whitening'])
 % Compute the covariance matrix of the extended signal
 covariance = (ext_SIG*ext_SIG')/length(ext_SIG);
 % Eigendecomposition of the covariance matrix of the extended signal
@@ -69,6 +71,7 @@ roa_ica_logc = zeros(num_MUs,1);
 true_spike_trains = [zeros(num_MUs, L) true_spike_trains];
 %% Loop over all motor units
 for mu_idx = 1:num_MUs
+    disp(['MU: ',num2str(mu_idx),'/',num2str(num_MUs)])
     %% Optimal decomposition using the model knowledge
     mu_response = squeeze(motor_unit_responses(mu_idx,:,:));
     % Transform the MU response into the optimal separation vector
@@ -104,88 +107,94 @@ for mu_idx = 1:num_MUs
     predicted_spike_trains_raw(mu_idx,:) = icasig;
     %% ICA Decomposition, contrast function: Skewness
     w = fixedpointalg(mu_response,whitened_signal,[],200,'skew');
-    [~,~,SIL_ica_skew(mu_idx)] = calcSIL(whitened_signal, w, Fs);
-    icasig = w'*whitened_signal.*abs(w'*whitened_signal);
-    [pks, locs] = findpeaks(icasig',  'MinPeakDistance', 0.02*Fs);
-    % K-means clustering
-    idx = kmeans(pks, 2, 'Start', [min(pks), max(pks)]');
-    [~, argmax] = max(pks);
-    active_class = idx(argmax);
-    tp_peaks = locs(idx == active_class);
-    ica_spike_train_binary = zeros(1, ext_SIG_Dim);
-    ica_spike_train_binary(tp_peaks) = 1;
-    ica_spike_train_binary = ica_spike_train_binary(1:ext_SIG_Dim - L + 1);
-    % Align the predicted spike train and the true spike train in time
-    [aligned_predicted_spike_train, aligned_true_spike_train, shift] = align_signals(...
-        ica_spike_train_binary, squeeze(true_spike_trains(mu_idx, :)), 1000);
-    aligned_predicted_spike_train = squeeze(aligned_predicted_spike_train);
-    aligned_true_spike_train = squeeze(aligned_true_spike_train);
-    % Clasify the predicted spikes into true positive firings (TP), false
-    % positive firings (FP) and false negative firings (FN). 
-    aligned_predicted_spike_train_tolerance = conv(aligned_predicted_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
-    aligned_true_spike_train_tolerance = conv(aligned_true_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
-    TP = sum(aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
-    FP = sum(aligned_predicted_spike_train .* ~aligned_true_spike_train_tolerance, 'all');
-    FN = sum( ~aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
-    % Compute the rate-of-agreement (roa)
-    roa_ica_skew(mu_idx) = TP/(TP+FP+FN);
-    icasig_skew(mu_idx,:) = icasig;
+    if all(isfinite(w))
+        [~,~,SIL_ica_skew(mu_idx)] = calcSIL(whitened_signal, w, Fs);
+        icasig = w'*whitened_signal.*abs(w'*whitened_signal);
+        [pks, locs] = findpeaks(icasig',  'MinPeakDistance', 0.02*Fs);
+        % K-means clustering
+        idx = kmeans(pks, 2, 'Start', [min(pks), max(pks)]');
+        [~, argmax] = max(pks);
+        active_class = idx(argmax);
+        tp_peaks = locs(idx == active_class);
+        ica_spike_train_binary = zeros(1, ext_SIG_Dim);
+        ica_spike_train_binary(tp_peaks) = 1;
+        ica_spike_train_binary = ica_spike_train_binary(1:ext_SIG_Dim - L + 1);
+        % Align the predicted spike train and the true spike train in time
+        [aligned_predicted_spike_train, aligned_true_spike_train, shift] = align_signals(...
+            ica_spike_train_binary, squeeze(true_spike_trains(mu_idx, :)), 1000);
+        aligned_predicted_spike_train = squeeze(aligned_predicted_spike_train);
+        aligned_true_spike_train = squeeze(aligned_true_spike_train);
+        % Clasify the predicted spikes into true positive firings (TP), false
+        % positive firings (FP) and false negative firings (FN). 
+        aligned_predicted_spike_train_tolerance = conv(aligned_predicted_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
+        aligned_true_spike_train_tolerance = conv(aligned_true_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
+        TP = sum(aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
+        FP = sum(aligned_predicted_spike_train .* ~aligned_true_spike_train_tolerance, 'all');
+        FN = sum( ~aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
+        % Compute the rate-of-agreement (roa)
+        roa_ica_skew(mu_idx) = TP/(TP+FP+FN);
+        icasig_skew(mu_idx,:) = icasig;
+    end
     %% ICA Decomposition, contrast function: Kurtosis
     w = fixedpointalg(mu_response,whitened_signal,[],200,'kurtosis');
-    [~,~,SIL_ica_kurt(mu_idx)] = calcSIL(whitened_signal, w, Fs);
-    icasig = w'*whitened_signal.*abs(w'*whitened_signal);
-    [pks, locs] = findpeaks(icasig',  'MinPeakDistance', 0.02*Fs);
-    % K-means clustering
-    idx = kmeans(pks, 2, 'Start', [min(pks), max(pks)]');
-    [~, argmax] = max(pks);
-    active_class = idx(argmax);
-    tp_peaks = locs(idx == active_class);
-    ica_spike_train_binary = zeros(1, ext_SIG_Dim);
-    ica_spike_train_binary(tp_peaks) = 1;
-    ica_spike_train_binary = ica_spike_train_binary(1:ext_SIG_Dim - L + 1);
-    % Align the predicted spike train and the true spike train in time
-    [aligned_predicted_spike_train, aligned_true_spike_train, shift] = align_signals(...
-        ica_spike_train_binary, squeeze(true_spike_trains(mu_idx, :)), 1000);
-    aligned_predicted_spike_train = squeeze(aligned_predicted_spike_train);
-    aligned_true_spike_train = squeeze(aligned_true_spike_train);
-    % Clasify the predicted spikes into true positive firings (TP), false
-    % positive firings (FP) and false negative firings (FN). 
-    aligned_predicted_spike_train_tolerance = conv(aligned_predicted_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
-    aligned_true_spike_train_tolerance = conv(aligned_true_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
-    TP = sum(aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
-    FP = sum(aligned_predicted_spike_train .* ~aligned_true_spike_train_tolerance, 'all');
-    FN = sum( ~aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
-    % Compute the rate-of-agreement (roa)
-    roa_ica_kurt(mu_idx) = TP/(TP+FP+FN);
-    icasig_kurt(mu_idx,:) = icasig;
+    if all(isfinite(w))
+        [~,~,SIL_ica_kurt(mu_idx)] = calcSIL(whitened_signal, w, Fs);
+        icasig = w'*whitened_signal.*abs(w'*whitened_signal);
+        [pks, locs] = findpeaks(icasig',  'MinPeakDistance', 0.02*Fs);
+        % K-means clustering
+        idx = kmeans(pks, 2, 'Start', [min(pks), max(pks)]');
+        [~, argmax] = max(pks);
+        active_class = idx(argmax);
+        tp_peaks = locs(idx == active_class);
+        ica_spike_train_binary = zeros(1, ext_SIG_Dim);
+        ica_spike_train_binary(tp_peaks) = 1;
+        ica_spike_train_binary = ica_spike_train_binary(1:ext_SIG_Dim - L + 1);
+        % Align the predicted spike train and the true spike train in time
+        [aligned_predicted_spike_train, aligned_true_spike_train, shift] = align_signals(...
+            ica_spike_train_binary, squeeze(true_spike_trains(mu_idx, :)), 1000);
+        aligned_predicted_spike_train = squeeze(aligned_predicted_spike_train);
+        aligned_true_spike_train = squeeze(aligned_true_spike_train);
+        % Clasify the predicted spikes into true positive firings (TP), false
+        % positive firings (FP) and false negative firings (FN). 
+        aligned_predicted_spike_train_tolerance = conv(aligned_predicted_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
+        aligned_true_spike_train_tolerance = conv(aligned_true_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
+        TP = sum(aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
+        FP = sum(aligned_predicted_spike_train .* ~aligned_true_spike_train_tolerance, 'all');
+        FN = sum( ~aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
+        % Compute the rate-of-agreement (roa)
+        roa_ica_kurt(mu_idx) = TP/(TP+FP+FN);
+        icasig_kurt(mu_idx,:) = icasig;
+    end
     %% ICA Decomposition, contrast function: LogCosh
     w = fixedpointalg(mu_response,whitened_signal,[],200,'logcosh');
-    [~,~,SIL_ica_logc(mu_idx)] = calcSIL(whitened_signal, w, Fs);
-    icasig = w'*whitened_signal.*abs(w'*whitened_signal);
-    [pks, locs] = findpeaks(icasig',  'MinPeakDistance', 0.02*Fs);
-    % K-means clustering
-    idx = kmeans(pks, 2, 'Start', [min(pks), max(pks)]');
-    [~, argmax] = max(pks);
-    active_class = idx(argmax);
-    tp_peaks = locs(idx == active_class);
-    ica_spike_train_binary = zeros(1, ext_SIG_Dim);
-    ica_spike_train_binary(tp_peaks) = 1;
-    ica_spike_train_binary = ica_spike_train_binary(1:ext_SIG_Dim - L + 1);
-    % Align the predicted spike train and the true spike train in time
-    [aligned_predicted_spike_train, aligned_true_spike_train, shift] = align_signals(...
-        ica_spike_train_binary, squeeze(true_spike_trains(mu_idx, :)), 1000);
-    aligned_predicted_spike_train = squeeze(aligned_predicted_spike_train);
-    aligned_true_spike_train = squeeze(aligned_true_spike_train);
-    % Clasify the predicted spikes into true positive firings (TP), false
-    % positive firings (FP) and false negative firings (FN). 
-    aligned_predicted_spike_train_tolerance = conv(aligned_predicted_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
-    aligned_true_spike_train_tolerance = conv(aligned_true_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
-    TP = sum(aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
-    FP = sum(aligned_predicted_spike_train .* ~aligned_true_spike_train_tolerance, 'all');
-    FN = sum( ~aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
-    % Compute the rate-of-agreement (roa)
-    roa_ica_logc(mu_idx) = TP/(TP+FP+FN);
-    icasig_logc(mu_idx,:) = icasig;
+    if all(isfinite(w))
+        [~,~,SIL_ica_logc(mu_idx)] = calcSIL(whitened_signal, w, Fs);
+        icasig = w'*whitened_signal.*abs(w'*whitened_signal);
+        [pks, locs] = findpeaks(icasig',  'MinPeakDistance', 0.02*Fs);
+        % K-means clustering
+        idx = kmeans(pks, 2, 'Start', [min(pks), max(pks)]');
+        [~, argmax] = max(pks);
+        active_class = idx(argmax);
+        tp_peaks = locs(idx == active_class);
+        ica_spike_train_binary = zeros(1, ext_SIG_Dim);
+        ica_spike_train_binary(tp_peaks) = 1;
+        ica_spike_train_binary = ica_spike_train_binary(1:ext_SIG_Dim - L + 1);
+        % Align the predicted spike train and the true spike train in time
+        [aligned_predicted_spike_train, aligned_true_spike_train, shift] = align_signals(...
+            ica_spike_train_binary, squeeze(true_spike_trains(mu_idx, :)), 1000);
+        aligned_predicted_spike_train = squeeze(aligned_predicted_spike_train);
+        aligned_true_spike_train = squeeze(aligned_true_spike_train);
+        % Clasify the predicted spikes into true positive firings (TP), false
+        % positive firings (FP) and false negative firings (FN). 
+        aligned_predicted_spike_train_tolerance = conv(aligned_predicted_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
+        aligned_true_spike_train_tolerance = conv(aligned_true_spike_train, ones(3,1), 'same'); % Tolerance of +/- 1 sample
+        TP = sum(aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
+        FP = sum(aligned_predicted_spike_train .* ~aligned_true_spike_train_tolerance, 'all');
+        FN = sum( ~aligned_predicted_spike_train_tolerance.*aligned_true_spike_train, 'all');
+        % Compute the rate-of-agreement (roa)
+        roa_ica_logc(mu_idx) = TP/(TP+FP+FN);
+        icasig_logc(mu_idx,:) = icasig;
+    end
 end 
 SIL.opt = SIL_opt;
 SIL.ica_skew = SIL_ica_skew;
