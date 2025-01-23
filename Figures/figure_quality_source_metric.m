763ede09630af4b2f5b51829bef3c35764ca8865
robinrohlen <67264249+robinrohlen@users.noreply.github.com>
2025-01-23 17:11:32 +0100

    updated quality source metric figure

--- Figures/figure_quality_source_metric.m
+++ Figures/figure_quality_source_metric.m
@@ -4,90 +4,101 @@ clearvars; close all;
 
 rng(0)
 
-cd '../LIF model/'
+useExistingData=1;
 
-I=7e-9; % 7 nA input current
-noise_dB=20;
-fs=2048;
+if useExistingData==0
+    cd '../LIF model/'
 
-[spike_times,time_param,membr_param,CI]=generate_spike_trains(I);
-[data,data_unfilt,sig_noise,muap]=generate_emg_signals(spike_times,time_param,noise_dB);
+    I=7e-9; % 7 nA input current
+    noise_dB=20;
+    fs=2048;
 
-addpath '../pure-simulation-trials/functions/'
+    [spike_times,time_param,membr_param,CI]=generate_spike_trains(I);
+    [data,data_unfilt,sig_noise,muap]=generate_emg_signals(spike_times,time_param,noise_dB);
 
-show_plots=0;
+    addpath '../pure-simulation-trials/functions/'
 
-% Select 64 out of 256 channels
-data=data(65:128,:);
-sig_noise=sig_noise(65:128,:);
-data_unfilt=data_unfilt(65:128,:);
+    show_plots=0;
 
-R=16; % extension factor
+    % Select 64 out of 256 channels
+    data=data(65:128,:);
+    sig_noise=sig_noise(65:128,:);
+    data_unfilt=data_unfilt(65:128,:);
 
-sil=zeros(1,size(spike_times,2));
-pnr=zeros(1,size(spike_times,2));
-skew=zeros(1,size(spike_times,2));
-kurt=zeros(1,size(spike_times,2));
-sep=zeros(4,size(spike_times,2));
+    R=16; % extension factor
 
-for i=1:size(spike_times,2) % MU selection
-    disp([num2str(i),'/',num2str(size(spike_times,2))]);
+    sil=zeros(1,size(spike_times,2));
+    pnr=zeros(1,size(spike_times,2));
+    skew=zeros(1,size(spike_times,2));
+    kurt=zeros(1,size(spike_times,2));
+    sep=zeros(4,size(spike_times,2));
 
-    % Extend and whiten
-    eSIG = extension(data,R);
-    [wSIG, whitening_matrix] = whitening(eSIG,'ZCA');
+    for i=1:size(spike_times,2) % MU selection
+        disp([num2str(i),'/',num2str(size(spike_times,2))]);
 
-    w = muap{i}(65:128,:);
-    w = extension(w,R);
-    w = whitening_matrix * w;
+        % Extend and whiten
+        eSIG = extension(data,R);
+        [wSIG, whitening_matrix] = whitening(eSIG,'ZCA');
 
-    % Reconstruction
-    sig=w'*wSIG;
-    % sig=sig./max(sig);
+        w = muap{i}(65:128,:);
+        w = extension(w,R);
+        w = whitening_matrix * w;
 
-    % Select the source with highest skewness
-    save_skew=zeros(1,size(sig,1));
-    for ind=1:size(sig,1)
-        save_skew(ind)=skewness(sig(ind,:));
+        % Reconstruction
+        sig=w'*wSIG;
+        % sig=sig./max(sig);
+
+        % Select the source with highest skewness
+        save_skew=zeros(1,size(sig,1));
+        for ind=1:size(sig,1)
+            save_skew(ind)=skewness(sig(ind,:));
+        end
+        [~,maxInd]=max(save_skew);
+        w = w(:,maxInd);
+        w = w./norm(w);
+
+        % Reconstruction
+        sig=w'*wSIG;
+
+        % Estimated spike times
+        est_spikes=est_spike_times(sig,fs);
+
+        % Compute SIL and PNR
+        sil(i)=compute_sil(sig,est_spikes);
+        pnr(i)=compute_pnr(sig,est_spikes,fs,[true,3],1);
+        skew(i)=skewness(sig);
+        kurt(i)=kurtosis(sig);
+        sep(:,i)=separability_metric(sig,spike_times{i});
+
+        % Make figure
+        if show_plots==1
+            t=tiledlayout(1,1);
+            figure(1);set(gcf,'units','points','position',[40,295,1824,545])
+            time_win=[0 60];
+
+            hold on;
+            plot(linspace(0,length(data)/fs,size(eSIG,2)),sig);
+            hold off;
+            set(gca,'TickDir','out');set(gcf,'color','w');set(gca,'FontSize',16);
+            xlim(time_win);
+            xticks(time_win(1):10:time_win(2));
+            title(['MU: ',num2str(i)],'FontWeight','normal');
+            xlabel('Time (s)');
+
+            t.TileSpacing='compact';
+            t.Padding='compact';
+
+            pause;
+        end
     end
-    [~,maxInd]=max(save_skew);
-    w = w(:,maxInd);
-    w = w./norm(w);
+    cd '../Figures/'
 
-    % Reconstruction
-    sig=w'*wSIG;
-
-    % Estimated spike times
-    est_spikes=est_spike_times(sig,fs);
-
-    % Compute SIL and PNR
-    sil(i)=compute_sil(sig,est_spikes);
-    pnr(i)=compute_pnr(sig,est_spikes,fs,[true,3],1);
-    skew(i)=skewness(sig);
-    kurt(i)=kurtosis(sig);
-    sep(:,i)=separability_metric(sig,spike_times{i});
-
-    % Make figure
-    if show_plots==1
-        t=tiledlayout(1,1);
-        figure(1);set(gcf,'units','points','position',[40,295,1824,545])
-        time_win=[0 60];
-
-        hold on;
-        plot(linspace(0,length(data)/fs,size(eSIG,2)),sig);
-        hold off;
-        set(gca,'TickDir','out');set(gcf,'color','w');set(gca,'FontSize',16);
-        xlim(time_win);
-        xticks(time_win(1):10:time_win(2));
-        title(['MU: ',num2str(i)],'FontWeight','normal');
-        xlabel('Time (s)');
-
-        t.TileSpacing='compact';
-        t.Padding='compact';
-
-        pause;
-    end
+    save('quality_source_metric.mat','sep','pnr','sil','skew')
 end
+
+clearvars
+
+load('quality_source_metric.mat')
 
 %% Make figure
 cmap=lines(4);
