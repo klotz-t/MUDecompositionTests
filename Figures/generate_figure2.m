1607a26fd254eb79249c99466182bfd4750bac16
robinrohlen <67264249+robinrohlen@users.noreply.github.com>
2025-01-31 16:28:37 +0100

    added code generate frequency current plot

--- Figures/generate_figure2.m
+++ Figures/generate_figure2.m
@@ -5,11 +5,13 @@
 
 clearvars; close all;
 
+% If 0 rerun simulation
+useExistingData=0;
+% If 1 plot the replication data
+useReplicationData=0;
+
 % Set number of motor neurons in the pool
 n_mn=300;
-
-% Load data
-load('frequency_current_relation.mat')
 
 % Set number of functional motor neuron clusters
 n_clust=1;
@@ -37,9 +39,40 @@ membr_param.tref = 2.7e-8./(motoneuron_soma_diameters.^1.51) .* 0.2;
 membr_param.Rm=(1.68e-10)./(3.96e-4.*(3.85e-9 .* 9.1.^(((1:n_mn)./n_mn).^1.1831)).^0.396).^2.43;
 % Membrane time constants (s)
 membr_param.tau_m=7.9e-5.*(motoneuron_soma_diameters.^1) .*  membr_param.Rm;
-% Gain parameters
-membr_param.gain_leak=linspace(0.75,0.85,n_mn);
+% Gain parameters (leakage and excitability)
+membr_param.gain_leak=linspace(0.25,0.15,n_mn); 
 membr_param.gain_exc=membr_param.gain_leak;
+
+% Set time parameters
+time_param.T_dur = 60; % Total duration (s)
+time_param.fs = 10e3; % 10 kHz
+time_param.dt = 1/time_param.fs; % Time step (s)
+time_param.T = 0:time_param.dt:time_param.T_dur; % Time vector
+
+if useExistingData==0
+    cd '../LIF model/'
+    current_range=linspace(5e-9,45e-9,1000);
+    firing_rates=zeros(length(current_range),n_mn);
+
+    for ind=1:length(current_range)
+        spike_times=lif_model(n_mn,current_range(ind)*ones(n_mn,size(time_param.T,2)),membr_param,time_param);
+        if size(spike_times,2) > 0
+            for tmp=1:size(spike_times,2)
+                firing_rates(ind,tmp)=median(1./diff(spike_times{tmp}/(1/time_param.dt)));
+            end
+        end
+    end
+    % Save data
+    cd '../Figures/'
+    save('my_data/frequency_current_relation.mat','current_range','firing_rates')
+end
+
+% Load data
+if useReplicationData
+    load('replication_data/frequency_current_relation.mat');
+else
+    load('my_data/frequency_current_relation.mat');
+end
 
 % Generate figure
 cmap=flip(turbo(n_mn));
@@ -90,6 +123,7 @@ set(gca,'FontSize',18);
 xlabel('Input current (nA)');
 ylabel('Firing rate (Hz)');
 xlim([0 45])
+ylim([0 50])
 h=legend('1st MN','50th MN','100th MN','150th MN','200th MN','250th MN','300th MN','location','northwest','NumColumns',2);
 h.Box='off';
 
